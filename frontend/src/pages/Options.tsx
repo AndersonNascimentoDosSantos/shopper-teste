cfd320dee43ff09443e89d687bc9ace0e52cdde3
Anderson Nascimento Dos Santos <anderson@nvgo.com.br>
2024-11-30 21:40:03 -0300

    options card

--- frontend/src/pages/Options.tsx
+++ frontend/src/pages/Options.tsx
@@ -34,21 +34,31 @@ const Options: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h2>Opções de Motoristas</h2>
       {/* Aqui você pode integrar um mapa estático usando uma biblioteca como Google Maps ou Leaflet */}
       <h3>Rota:</h3>
       {/* Exibir informações da rota */}
-      {options.map((driver: any) => (
-        <div key={driver.id}>
-          <h4>{driver.name}</h4>
-          <p>{driver.description}</p>
-          <p>{driver.vehicle}</p>
-          <p>Avaliação: {driver.review.rating}</p>
-          <p>Valor Total: R$ {driver.value.toFixed(2)}</p>
-          <button onClick={() => handleConfirm(driver)}>Escolher</button>
-        </div>
-      ))}
+      <div className="flex flex-wrap gap-5 justify-evenly">
+        {options.map((driver: any) => (
+          <div
+            key={driver.id}
+            className="flex flex-col border  border-s-slate-800 rounded-lg w-96 gap-2 p-5"
+          >
+            <h4>{driver.name}</h4>
+            <p>{driver.description}</p>
+            <p>{driver.vehicle}</p>
+            <p>Avaliação: {driver.review.rating}</p>
+            <p>Valor Total: R$ {driver.value.toFixed(2)}</p>
+            <button
+              className="bg-green-400 rounded-2xl"
+              onClick={() => handleConfirm(driver)}
+            >
+              Escolher
+            </button>
+          </div>
+        ))}
+      </div>
       {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
